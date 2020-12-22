692de5260ed8da448ac2d26cd9cd792455002e03
xhico <franc.filipe1996@gmail.com>
2020-12-22 01:40:53 +0000

    Update Blackjack.c

--- BlackjackV2/Blackjack.c
+++ BlackjackV2/Blackjack.c
@@ -4,6 +4,8 @@
 
 #define DECK_SIZE 52;
 
+
+
 int main() {
 	printf("# ---------------- #\n");
 	printf("# Blackjack C Game #\n");
@@ -11,10 +13,12 @@ int main() {
 	printf("# ---------------- #\n\n");
 
 	char pName[6];
+	int bankroll;
+
+
 	printf("Player Name: ");
 	scanf_s("%s", pName, sizeof(pName));
 
-	int bankroll;
 	printf("Insert bankroll: ");
 	scanf_s("%d", &bankroll);
 
